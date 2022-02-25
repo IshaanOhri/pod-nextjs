881df7ec54672c7f1f58370e06a325d8f4fd1bb6
Ishaan Ohri <ishaan99ohri@gmail.com>
2022-02-25 16:31:25 +0530

    Check mobile app

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { login } from "@inrupt/solid-client-authn-browser";
+import {
+  login,
+  handleIncomingRedirect,
+  fetch,
+  getDefaultSession,
+  onLogin,
+} from "@inrupt/solid-client-authn-browser";
+
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
   const signInPOD = async (e) => {
     e.preventDefault();
     console.log("Button Clicked");
 
     await login({
+      redirectUrl: "http://localhost:3000/dashboard",
       oidcIssuer: "https://inrupt.net",
       clientName: "POD NextJS Test",
     });
@@ -18,9 +29,11 @@ export default function Home() {
 
   return (
     <div>
+      <h1>Home Page</h1>
       <button
         onClick={(e) => {
-          signInPOD(e);
+          // signInPOD(e);
+          router.push("/dashboard");
         }}
       >
         Sign In
